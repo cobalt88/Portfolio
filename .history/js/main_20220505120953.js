814604fa714a1aa751212c3c928349038ce26d1b
cobalt88 <vincentteune@gmail.com>
2022-05-07 06:20:12 -0400

    5.7.22
    not much has changed, still playing around with it

--- .history/js/main_20220505120953.js
+++ .history/js/main_20220505120953.js
@@ -59,27 +59,28 @@ $(document).ready(function($) {
 		$('.full-height').fullHeight();
 	});
 	
-	var counter = () => {
+	var counter = function() {
+		
+		$('#section-counter, .ftco-about').waypoint( function( direction ) {
 
-		$('#section-counter, .ftco-about').waypoint(function (direction) {
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
 
-			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
-				$('.number').each(function () {
-					var $this = $(this), num = $this.data('number');
-					console.log(num);
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+						console.log(num);
 					$this.animateNumber(
-						{
-							number: num,
-							numberStep: comma_separator_number_step
-						}, 7000
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
 					);
 				});
-
+				
 			}
 
-		}, { offset: '95%' });
+		} , { offset: '95%' } );
 
 	}
 	counter();
